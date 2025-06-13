79eef6d3e229a69715d9b59ccf204d0f2d215b45
igrisdev <johanalvarezz237@gmail.com>
2025-06-13 13:01:19 -0500

    Enhance MainUserScreen layout with Google Fonts and update body structure; add path_provider dependency in pubspec.yaml

--- lib/screens/users/main_user_screen.dart
+++ lib/screens/users/main_user_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:reportes_unimayor/themes/light.theme.dart';
 
 class MainUserScreen extends StatelessWidget {
@@ -7,16 +8,29 @@ class MainUserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           'Reportes UniMayor',
-          style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
+          style: GoogleFonts.poppins(fontWeight: FontWeight.w600, fontSize: 18),
         ),
       ),
       drawer: drawerUser(),
-      body: Column(children: []),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
+        child: Center(
+          child: Text(
+            "Sin Reporte",
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.transparent,
         elevation: 0,
@@ -32,7 +46,10 @@ class MainUserScreen extends StatelessWidget {
             children: [
               Text(
                 "Realizar Reporte",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
               ),
               IconButton(icon: const Icon(Icons.send), onPressed: () {}),
             ],
