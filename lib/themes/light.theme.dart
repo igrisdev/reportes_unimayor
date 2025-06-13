87ae99e6d1c77ade2835d115a9928bdad97a5b53
igrisdev <johanalvarezz237@gmail.com>
2025-06-12 23:51:01 -0500

    Add light theme configuration for Flutter application

---
+++ lib/themes/light.theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: const Color(0xFF195A90),
+    primaryFixed: const Color(0xFF034593),
+    primaryFixedDim: const Color(0xFF8DC5F3),
+    secondary: const Color(0xFFFFCF01),
+    secondaryFixed: const Color(0xFF000000),
+    tertiary: const Color.fromARGB(255, 255, 255, 255),
+  ),
+);
