a50462d41fcec378fbe4d3cdf58594e80770b896
igrisdev <johanalvarezz237@gmail.com>
2025-06-15 17:54:56 -0500

    Update padding and text content in MainUserScreen for better layout consistency

--- lib/screens/users/main_user_screen.dart
+++ lib/screens/users/main_user_screen.dart
@@ -41,7 +41,7 @@ class MainUserScreen extends StatelessWidget {
                     ),
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(11),
                     child: Column(
                       children: [
                         Row(
@@ -60,12 +60,12 @@ class MainUserScreen extends StatelessWidget {
                             ),
                           ],
                         ),
-                        SizedBox(height: 10),
+                        SizedBox(height: 13),
                         Text(
-                          "lorem fa ipsum r sit amet consectetur adipisicing elit. dolor sit amet consectetur adipisicing elit. Voluptate, quia",
+                          "lorem fa ipsum r sit amet consectetur adipisicing elit. dolor sit amet consectetur adipisicing elit. Voluptate, quia lorem fa ipsum r sit amet consectetur adipisicing elit. dolor sit amet consectetur adipisicing elit. Voluptate, quia",
                           style: GoogleFonts.poppins(fontSize: 14),
                         ),
-                        SizedBox(height: 10),
+                        SizedBox(height: 13),
                         Row(
                           children: [
                             Container(
