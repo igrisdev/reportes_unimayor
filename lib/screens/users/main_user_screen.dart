09ed432e4eab9011430dac2e1eb8d602092492c7
igrisdev <johanalvarezz237@gmail.com>
2025-06-16 12:02:41 -0500

    Refactor bottom navigation to use context for navigation to CreateReportUserScreen; update BottomAppBar to include tap functionality

--- lib/screens/users/main_user_screen.dart
+++ lib/screens/users/main_user_screen.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:reportes_unimayor/components/app_bar_user.dart';
 import 'package:reportes_unimayor/components/card_report.dart';
 import 'package:reportes_unimayor/components/drawer_user.dart';
+import 'package:reportes_unimayor/screens/users/create_report_user_screen.dart';
 import 'package:reportes_unimayor/screens/users/view_report_user_screen.dart';
 import 'package:reportes_unimayor/themes/light.theme.dart';
 
@@ -43,33 +44,44 @@ class MainUserScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: bottomAppBarMain(),
+      bottomNavigationBar: bottomAppBarMain(context),
     );
   }
 
-  BottomAppBar bottomAppBarMain() {
-    return BottomAppBar(
-      color: Colors.transparent,
-      elevation: 0,
-      height: 90,
-      child: Container(
-        decoration: BoxDecoration(
-          color: lightMode.colorScheme.secondary,
-          borderRadius: BorderRadius.circular(100),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Realizar Reporte",
-              style: GoogleFonts.poppins(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+  GestureDetector bottomAppBarMain(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const CreateReportUserScreen(),
+          ),
+        );
+      },
+      child: BottomAppBar(
+        color: Colors.transparent,
+        elevation: 0,
+        height: 90,
+        child: Container(
+          decoration: BoxDecoration(
+            color: lightMode.colorScheme.secondary,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Realizar Reporte",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
               ),
-            ),
-            IconButton(icon: const Icon(Icons.send), onPressed: () {}),
-          ],
+              SizedBox(width: 10),
+              Icon(Icons.send),
+            ],
+          ),
         ),
       ),
     );
