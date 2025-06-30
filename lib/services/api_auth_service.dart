604126c8e686e48b1c3979dbabd671e798f2eece
igrisdev <johanalvarezz237@gmail.com>
2025-06-30 18:06:46 -0500

    Update auth service and API configuration
    The changes involve updating the authentication service implementation,
    adding user type detection, configuring the API base URL for Android
    emulator support, and ignoring .zed files. Update auth flow and API
    configuration
    
    - Change login credentials to use institutional email - Add user role
    detection functionality - Update API base URL for Android emulator
    support - Add JWT token dependency - Ignore .zed files in git

--- lib/services/api_auth_service.dart
+++ lib/services/api_auth_service.dart
@@ -1,15 +1,33 @@
 import 'package:reportes_unimayor/services/base_dio_service.dart';
 
 class ApiAuthService extends BaseDioService {
-
   Future<String> login(String email, String password) async {
     final response = await dio.post(
       '/auth/login',
-      data: {'email': email, 'password': password},
+      data: {'correoInstitucional': email, 'contraseña': password},
     );
 
-    print(response.data);
+    if (response.statusCode == 200) {
+      return response.data['token'];
+    } else {
+      throw Exception('Error al iniciar sesión');
+    }
+  }
 
-    return response.data['token'];
+  Future<String> typeUser(String token) async {
+    // final decodedToken = JwtDecoder.decode(token);
+    // final roles = decodedToken['roles'];
+
+    final roles = ['Usuario', 'Brigadista', 'Administrador'];
+
+    if (roles.contains('Usuario')) {
+      return 'Usuario';
+    } else if (roles.contains('Brigradista')) {
+      return 'Brigadista';
+    } else if (roles.contains('Administrador')) {
+      return 'Administrador';
+    } else {
+      return 'Usuario';
+    }
   }
 }
--- lib/services/base_dio_service.dart
+++ lib/services/base_dio_service.dart
@@ -2,7 +2,8 @@ import 'package:dio/dio.dart';
 
 class BaseDioService {
   static final optionsDio = BaseOptions(
-    baseUrl: 'https://localhost:7208/api',
+    baseUrl: 'http://10.0.2.2:5217/api',
+    // baseUrl: 'http://localhost:5217/api', // Descomenta esta si no usas emulador Android
     connectTimeout: Duration(seconds: 5),
     receiveTimeout: Duration(seconds: 3),
   );
