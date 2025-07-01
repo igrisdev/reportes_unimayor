20c526bf022212402923ef869c7a27b3c637be29
igrisdev <johanalvarezz237@gmail.com>
2025-06-30 22:33:06 -0500

    Add initial routing configuration and assets

---
+++ lib/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:go_router/go_router.dart';
+import 'package:reportes_unimayor/screens/auth/auth_screen.dart';
+import 'package:reportes_unimayor/screens/users/main_user_screen.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  initialLocation: '/auth',
+  routes: [
+    GoRoute(path: '/auth', builder: (context, state) => AuthScreen()),
+    GoRoute(path: '/user', builder: (context, state) => MainUserScreen()),
+  ],
+);
