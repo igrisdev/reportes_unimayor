221c1be93c8e29b87aff4d205a0d85ded0e146ce
igrisdev <johanalvarezz237@gmail.com>
2025-07-02 23:26:43 -0500

    Change drawer logout to use Riverpod and GoRouter

--- lib/components/drawer_user.dart
+++ lib/components/drawer_user.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:reportes_unimayor/screens/auth/auth_screen.dart';
+import 'package:reportes_unimayor/providers/token_provider.dart';
 import 'package:reportes_unimayor/screens/users/history_user_screen.dart';
 import 'package:reportes_unimayor/screens/users/main_user_screen.dart';
 
-class DrawerUser extends StatelessWidget {
+class DrawerUser extends ConsumerWidget {
   final BuildContext context;
 
   const DrawerUser({super.key, required this.context});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = GoRouter.of(context);
+
     return Drawer(
       child: Column(
         children: [
@@ -68,10 +72,9 @@ class DrawerUser extends StatelessWidget {
               ),
             ),
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const AuthScreen()),
-              );
+              ref.read(tokenProvider.notifier).removeToken();
+
+              router.go('/auth');
             },
           ),
 
