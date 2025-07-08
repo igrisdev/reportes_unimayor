ec8b7b5ff9e23a53bedf2063181a0cccbc548718
igrisdev <johanalvarezz237@gmail.com>
2025-07-08 00:43:48 -0500

    Refactor AuthScreen layout for improved UI and user experience; update SnackBar styling and login logic

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:reportes_unimayor/providers/is_brigadier_provider.dart';
 import 'package:reportes_unimayor/providers/token_provider.dart';
 import 'package:reportes_unimayor/services/api_auth_service.dart';
@@ -34,8 +35,11 @@ class _AuthScreenState extends ConsumerState<AuthScreen> {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red.shade700,
         duration: const Duration(seconds: 3),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: const EdgeInsets.all(10),
       ),
     );
   }
@@ -43,104 +47,169 @@ class _AuthScreenState extends ConsumerState<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              child: Image.asset('assets/icons/logo_unimayor.png', width: 100),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset('assets/icons/logo_unimayor.png', height: 100),
+                const SizedBox(height: 20),
+                Text(
+                  'Bienvenido',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Inicia sesión para continuar',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: const Icon(Icons.email_outlined),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, introduce un email';
+                          }
+                          // if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                          //   return 'Por favor, introduce un email válido';
+                          // }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Contraseña',
+                          prefixIcon: const Icon(Icons.lock_outline),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, introduce una contraseña';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 40),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 200),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 16,
+                              horizontal: 24,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            backgroundColor: const Color(
+                              0xFF003366,
+                            ), // Azul oscuro
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: _isLoading ? null : _login,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Iniciar sesión',
+                                style: GoogleFonts.poppins(fontSize: 16),
+                              ),
+                              if (_isLoading) const SizedBox(width: 20),
+                              if (_isLoading)
+                                SizedBox(
+                                  height: 24,
+                                  width: 24,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                    strokeWidth: 3,
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 10),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'Email',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, introduzca un email';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Contraseña',
-                      hintText: 'Contraseña',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, introduzca una contraseña';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: _isLoading
-                        ? null
-                        : () async {
-                            if (!_formKey.currentState!.validate()) return;
-
-                            final router = GoRouter.of(context);
-
-                            setState(() => _isLoading = true);
-
-                            try {
-                              final email = _emailController.text.trim();
-                              final password = _passwordController.text;
-
-                              final String? token = await ApiAuthService()
-                                  .login(email, password);
-
-                              if (token == null) {
-                                _showError('Credenciales incorrectas');
-                                return;
-                              }
-
-                              String? deviceToken = await FirebaseMessaging
-                                  .instance
-                                  .getToken();
-
-                              if (deviceToken != null) {
-                                await ApiTokenDeviceService().setTokenDevice(
-                                  deviceToken,
-                                  token,
-                                );
-                              }
-
-                              ref.read(tokenProvider.notifier).setToken(token);
-
-                              final userType = ref.read(isBrigadierProvider);
-
-                              if (userType) {
-                                router.go('/brigadier');
-                              } else {
-                                router.go('/user');
-                              }
-                            } catch (e) {
-                              _showError('Error, Intenta nuevamente.');
-                            } finally {
-                              setState(() => _isLoading = false);
-                            }
-                          },
-                    child: Text('Iniciar sesión'),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  // Se extrajo la lógica a una función para mayor claridad
+  void _login() async {
+    if (!_formKey.currentState!.validate()) return;
+
+    final router = GoRouter.of(context);
+    setState(() => _isLoading = true);
+
+    try {
+      final email = _emailController.text.trim();
+      final password = _passwordController.text;
+
+      final String? token = await ApiAuthService().login(email, password);
+
+      if (token == null) {
+        _showError('Credenciales incorrectas');
+        return;
+      }
+
+      String? deviceToken = await FirebaseMessaging.instance.getToken();
+
+      if (deviceToken != null) {
+        await ApiTokenDeviceService().setTokenDevice(deviceToken, token);
+      }
+
+      ref.read(tokenProvider.notifier).setToken(token);
+      final userType = ref.read(isBrigadierProvider);
+
+      // El 'mounted' check es importante antes de navegar después de un 'await'
+      if (!mounted) return;
+
+      if (userType) {
+        router.go('/brigadier');
+      } else {
+        router.go('/user');
+      }
+    } catch (e) {
+      _showError('Error de conexión. Intenta nuevamente.');
+    } finally {
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+    }
   }
 }
